51dec043e0facf204fce36536042b8dead4fc7d6
Andrew Udvare <audvare@gmail.com>
2026-03-21 16:35:27 -0400

    docs/conf: switch to sphinx-immaterial theme
    Sync settings with deltona docs config. Add sphinx_datatables,
    sphinx_immaterial, sphinxcontrib.jquery extensions and full
    theme options. Expand intersphinx mapping for project deps.
    
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 """See https://www.sphinx-doc.org/en/master/usage/configuration.html."""
+
 from __future__ import annotations
 
 from datetime import datetime, timezone
@@ -12,7 +13,12 @@ with (Path(__file__).parent.parent / 'pyproject.toml').open(newline='\n', encodi
     project_ = tomlkit.load(f).unwrap()['project']
     authors_list, name, version = itemgetter('authors', 'name', 'version')(project_)
 authors = [f'{d["name"]} <{d["email"]}>' for d in authors_list]
+# region Path setup
+# If extensions (or modules to document with autodoc) are in another directory, add these
+# directories to sys.path here. If the directory is relative to the documentation root, use
+# str(Path().parent.parent) to make it absolute, like shown here.
 sys.path.insert(0, str(Path(__file__).parent.parent))
+# endregion
 author = f'{authors_list[0]["name"]} <{authors_list[0]["email"]}>'
 copyright = str(datetime.now(timezone.utc).year)  # noqa: A001
 project = name
@@ -21,7 +27,77 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
-    'sphinx_click',
+    'sphinx_datatables',
+    'sphinx_immaterial',
+    'sphinxcontrib.jquery',
 ]
-html_theme = 'alabaster'
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+extensions += ['sphinx_click']
+datatables_class = 'sphinx-datatable'
+datatables_options = {'paging': 0}
+datatables_version = '1.13.4'
+html_theme = 'sphinx_immaterial'
+html_theme_options = {
+    'edit_uri': '/tree/master/docs',
+    'features': [
+        'announce.dismiss',
+        'content.action.edit',
+        'content.action.view',
+        'content.code.copy',
+        'content.tabs.link',
+        'content.tooltips',
+        'navigation.expand',
+        'navigation.footer',
+        'navigation.sections',
+        'navigation.top',
+        'search.share',
+        'search.suggest',
+        'toc.follow',
+        'toc.sticky',
+    ],
+    'globaltoc_collapse': True,
+    'icon': {
+        'edit': 'material/file-edit-outline',
+        'repo': 'fontawesome/brands/github'
+    },
+    'palette': [
+        {
+            'media': '(prefers-color-scheme)',
+            'toggle': {
+                'icon': 'material/brightness-auto',
+                'name': 'Switch to light mode'
+            },
+        },
+        {
+            'accent': 'light-blue',
+            'media': '(prefers-color-scheme: light)',
+            'primary': 'teal',
+            'scheme': 'default',
+            'toggle': {
+                'icon': 'material/lightbulb',
+                'name': 'Switch to dark mode'
+            },
+        },
+        {
+            'accent': 'blue',
+            'media': '(prefers-color-scheme: dark)',
+            'primary': 'black',
+            'scheme': 'slate',
+            'toggle': {
+                'icon': 'material/lightbulb-outline',
+                'name': 'Switch to system preference'
+            },
+        },
+    ],
+    'repo_name': 'ripcd',
+    'repo_url': 'https://github.com/Tatsh/ripcd',
+    'site_url': 'https://ripcd.readthedocs.org',
+    'toc_title_is_page_title': True,
+}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'bascom': ('https://bascom.readthedocs.io/en/latest/', None),
+    'click': ('https://click.palletsprojects.com/en/latest/', None),
+    'deltona': ('https://deltona.readthedocs.io/en/latest/', None),
+    'requests': ('https://requests.readthedocs.io/en/latest/', None),
+    'typing-extensions': ('https://typing-extensions.readthedocs.io/en/latest/', None),
+}
